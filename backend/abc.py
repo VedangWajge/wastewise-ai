5bab826729cf1cbb973e772f9109502db44e1221
Riya Varyani <varyaniriya2@gmail.com>
2025-10-13 10:54:07 +0530

    Added dataset folder inside backend

--- backend/abc.py
+++ backend/abc.py
@@ -2,7 +2,7 @@ print("[INFO] Starting model load...")
 
 from tensorflow.keras.models import load_model
 
-model_path = "backend/models/garbage_cnn_model.h5"
+model_path = "D:\WasteWiseAi\wastewise-ai\backend\models\garbage_cnn_model.h5"
 
 try:
     model = load_model(model_path)
